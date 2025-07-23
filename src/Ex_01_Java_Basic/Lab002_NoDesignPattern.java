9cdd34679bc8a2b8c7a5d04116e7bd963127a666
Brij Shingala <brij.shingala02@gmail.com>
2025-07-24 00:30:19 +0530

    6-Mar I am pushing code

---
+++ src/Ex_01_Java_Basic/Lab002_NoDesignPattern.java
@@ -0,0 +1,11 @@
+package Ex_01_Java_Basic;
+
+public class Lab002_NoDesignPattern {
+    public static void main(String[] args) {
+        // This is a comment
+        System.out.println("Hello This is comment program");
+        System.out.println("Hello This is comment program");
+
+    }
+}
+
